eedd20afa99e111041e8d21a0b42882222a610a1
bizhanmedet <128805878+bizhanmedet@users.noreply.github.com>
2023-05-09 17:26:18 +0600

    third

--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import Cart from './pages/Cart';
 
 function App() {
   return (
     <div className="wrapper">
       <Header />
-      <div className="content"></div>
-      <Routes>
-        <Route path="" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <div className="content">
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/Cart" element={<Cart />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,23 +15,22 @@ const Home = () => {
       .then((arr) => {
         setItems(arr);
         setIsLoading(false);
+        window.scrollTo(0, 0);
       });
   }, []);
   return (
-    <>
-      <div className="container">
-        <div className="content__top">
-          <Categories />
-          <Sort />
-        </div>
-        <h2 className="content__title">Все пиццы</h2>
-        <div className="content__items">
-          {isLoading
-            ? [...new Array(6)].map((_, index) => <Skeletone key={index} />)
-            : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-        </div>
+    <div className="container">
+      <div className="content__top">
+        <Categories />
+        <Sort />
       </div>
-    </>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeletone key={index} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </div>
   );
 };
 
